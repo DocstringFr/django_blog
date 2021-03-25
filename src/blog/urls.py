129177356ea94fa8c5c68b2100173a43da511ece
Thibault <houdon.thibault@gmail.com>
2021-03-25 14:49:47 +0100

    Add blogpost thumbnail

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,7 +1,9 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from blog import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('posts.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/posts/models.py
+++ src/posts/models.py
@@ -14,6 +14,7 @@ class BlogPost(models.Model):
     created_on = models.DateField(blank=True, null=True)
     published = models.BooleanField(default=False, verbose_name="Publié")
     content = models.TextField(blank=True, verbose_name="Contenu")
+    thumbnail = models.ImageField(blank=True, upload_to='blog')
 
     class Meta:
         ordering = ['-created_on']
